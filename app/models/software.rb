95604b0505f13007763ccf596020f2b34fa97aab
pete <kouyoumdjianps@masters.edu>
2023-10-17 01:50:16 +0000

    completed Software usage table

--- app/models/software.rb
+++ app/models/software.rb
@@ -1,3 +1,8 @@
 class Software < ApplicationRecord
   has_and_belongs_to_many :employees
+
+  def compliant?
+    #licence count is greater or than equal to user count
+    license_count >= employees.count
+  end
 end
