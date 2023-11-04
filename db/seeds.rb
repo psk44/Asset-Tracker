bb19a818dc2e59cdc706c5e4ab883d7c88e76e9f
pete <kouyoumdjianps@masters.edu>
2023-11-04 23:23:32 +0000

    seed file updated

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,58 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+# Seed device categories
+device_categories = ["Smart Phone", "Laptop", "Desktop"]
+device_categories.each do |category|
+  DeviceCategory.create(name: category)
+end
+
+# Seed device manufacturers
+device_manufacturers = [
+  { name: "Apple", website: "https://www.apple.com" },
+  { name: "Lenovo", website: "https://www.lenovo.com" },
+  { name: "Dell", website: "https://www.dell.com" }
+]
+device_manufacturers.each do |manufacturer_data|
+  DeviceManufacturer.create(manufacturer_data)
+end
+
+# Seed employees
+employees = []
+5.times do |n|
+  employees << Employee.create(
+    first_name: "Employee",
+    last_name: (n + 1).to_s
+  )
+end
+
+# Seed devices and assign to employees
+employees.each do |employee|
+  Device.create(
+    name: "Smart Phone #{employee.id}",
+    device_category: DeviceCategory.first,
+    device_manufacturer: DeviceManufacturer.first,
+    employee: employee
+  )
+  Device.create(
+    name: "Laptop #{employee.id}",
+    device_category: DeviceCategory.second,
+    device_manufacturer: DeviceManufacturer.second,
+    employee: employee
+  )
+end
+
+# Seed software packages
+softwares = []
+5.times do |n|
+  softwares << Software.create(
+    name: "Software #{n + 1}",
+    license_count: rand(1..5)
+  )
+end
+
+# Repeat the software assignment process 3 times
+3.times do
+  employees.each do |employee|
+    software_id = rand(softwares.length)
+    employee.softwares << softwares[software_id]
+  end
+end
+
