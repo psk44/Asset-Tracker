4661a1751673d8f13790e0f6ca95f00b37141655
pete <kouyoumdjianps@masters.edu>
2023-11-09 01:21:04 +0000

    misc changes

--- app/models/device.rb
+++ app/models/device.rb
@@ -1,5 +1,5 @@
 class Device < ApplicationRecord
-  belongs_to :employee
+  belongs_to :employee, optional: true
   belongs_to :device_manufacturer
   belongs_to :device_category
   has_many :softwares
--- config/routes.rb
+++ config/routes.rb
@@ -5,8 +5,8 @@ Rails.application.routes.draw do
   resources :softwares
   resources :devices
   resources :employees
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
+  get 'search', :to => "search#index"
+
   root "dashboard#index"
 end
