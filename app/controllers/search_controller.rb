e3c8d9c8b2a2f27a4f01b04b00c39f69fdad324f
pete <kouyoumdjianps@masters.edu>
2023-11-29 07:26:45 +0000

    finished filtering feature

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -6,7 +6,7 @@ class SearchController < ApplicationController
 
     @devices = Device.all
     @devices = @devices.where("name LIKE ?", "#{@query}%") if @query.present?
-    @devices = @devices.where(category_id: @category_id) if @category_id.present?
-    @devices = @devices.where(manufacturer_id: @manufacturer_id) if @manufacturer_id.present?
+    @devices = @devices.where(device_category_id: @category_id) if @category_id.present?
+    @devices = @devices.where(device_manufacturer_id: @manufacturer_id) if @manufacturer_id.present?
   end
 end
