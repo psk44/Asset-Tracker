0e14170591eedf9d840a7c3b21aceb052f66940d
pete <kouyoumdjianps@masters.edu>
2023-11-29 05:52:50 +0000

    fixing filter features

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,8 +1,12 @@
 class SearchController < ApplicationController
   def index
     @query = params[:query]
-    @employee_id = params[:employee_id]
+    @category_id = params[:category_id]
+    @manufacturer_id = params[:manufacturer_id]
 
-    @devices = Device.where("name like '#{@query}%'")
+    @devices = Device.all
+    @devices = @devices.where("name LIKE ?", "#{@query}%") if @query.present?
+    @devices = @devices.where(category_id: @category_id) if @category_id.present?
+    @devices = @devices.where(manufacturer_id: @manufacturer_id) if @manufacturer_id.present?
   end
 end
